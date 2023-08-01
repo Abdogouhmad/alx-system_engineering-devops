4b8a6a9935684bd806d15f9ffceb5e60cab44deb
div-styl <gouhmada@gmail.com>
2023-08-01 11:28:05 +0100

    small update

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 # a Ruby script that accepts one argument and pass it to a regular expression matching method
-puts ARGV[0].scan(/hbt{2.5}n/).join
+puts ARGV[0].scan(/hbt{2,5}n/).join
