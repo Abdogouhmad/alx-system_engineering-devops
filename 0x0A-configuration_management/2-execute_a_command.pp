5983cf40c388751808e793a12e5a0507787f73db
div-styl <gouhmada@gmail.com>
2023-09-22 11:55:17 -0700

    updated

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # execute cmd with puppet
 exec { 'pkill':
-	commadn => 'pkill -9 -f killmenow',
-	path => ['/usr/bin', '/usr/sbin', '/bin']
+  command => 'pkill -9 -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
