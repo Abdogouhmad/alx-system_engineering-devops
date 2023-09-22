7566c64b5bbaba139ac0be0ed5246e6931bcf9c0
div-styl <gouhmada@gmail.com>
2023-09-22 11:46:06 -0700

    styl updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # create file in /tmp
 file { '/tmp/school':
-
-	ensure => file,
-	path   => '/tmp/school',
-	mode   => '0744',
-	owner  => 'www-data',
-	group  => 'www-data',
-	content => 'I love Puppet'
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
