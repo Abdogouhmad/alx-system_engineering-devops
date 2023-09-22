03f77181e8e8114f98776ad47be0c003476555cb
div-styl <gouhmada@gmail.com>
2023-09-22 11:36:29 -0700

    updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,9 @@
 file { '/tmp/school':
 
 	ensure => file,
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
+	path   => '/tmp/school',
+	mode   => '0744',
+	owner  => 'www-data',
+	group  => 'www-data',
 	content => 'I love Puppet',
 }
