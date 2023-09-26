99356db19843d9797950a51f1651bc4f40087a69
div-styl <gouhmada@gmail.com>
2023-09-26 20:02:33 +0100

    tabs

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,24 @@
 # update ubuntu packages
 
 exec { 'update system':
-    command => '/usr/bin/apt-get -y update',
+	command => '/usr/bin/apt-get -y update',
 }
 
 package { 'nginx':
-    ensure => 'installed',
-    require => Exec['update system']
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
 file {'var/www/html/index.nginx-debian.html':
-    content => 'Hello World!',
+	content => 'Hello World!',
 }
 
 exec {'redirect_me':
-    command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-    provider => 'shell'
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
 service { 'nginx':
-    ensure => running,
-    require => package['nginx']
+	ensure => running,
+	require => package['nginx']
 }
