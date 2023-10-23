8dec2a9da595e5dc57f121510713784fb21068ae
div-styl <gouhmada@gmail.com>
2023-10-23 12:06:38 +0100

    Update!

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,12 +6,16 @@ import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(user_id)).json()
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                         .format(user_id)).json()
-    print("Employee {} is done with tasks({}/{}):".
-          format(user.get("name"), len(todos), len(todos)))
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    ).json()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
+    ).json()
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            user.get("name"), len(todos), len(todos)
+        )
+    )
     for todo in todos:
         print("\t {}".format(todo.get("title")))
-
