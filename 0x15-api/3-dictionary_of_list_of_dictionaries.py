209ffb3b1b1eb4f0a1f42ff3eaf754ac1cb2ca26
div-styl <gouhmada@gmail.com>
2023-10-23 12:32:34 +0100

    Update!

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""Exports to-do list information for all employees to a JSON format"""
+
+import json
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as file:
+        json.dump(
+            {
+                user.get("id"): [
+                    {
+                        "task": task.get("title"),
+                        "completed": task.get("completed"),
+                        "username": user.get("username"),
+                    }
+                    for task in requests.get(
+                        url + "todos", params={"userId": user.get("id")}
+                    ).json()
+                ]
+                for user in users
+            },
+            file,
+        )
