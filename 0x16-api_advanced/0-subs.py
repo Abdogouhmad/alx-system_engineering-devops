e5d86c64829b27e2a47009af71399f5401b5b79a
div-styl <gouhmada@gmail.com>
2023-11-07 18:35:19 +0100

    Update!

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    response = requests.get(url, headers={'User-Agent': 'Alx'})
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+Write a function that queries the Reddit
+API and prints the titles of
+the first 10 hot posts listed for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Prints the titles of the first 10 hot posts
+    listed for a given subreddit.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Alx'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children'][:10]:
+            print(post['data']['title'])
+    else:
+        print('None')
